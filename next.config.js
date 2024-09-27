5ac18c7f03cc595502ccad5bb3d6307caf5b2d1d
Thogsit <git@thogs.de>
2024-09-27 12:26:33 +0200

    ci: Switch to .js config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
   output: "export",
   basePath: "/mshack-organigram",
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
-export default nextConfig;
+//export default nextConfig;
+module.exports = nextConfig;
