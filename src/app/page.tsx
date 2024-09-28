599565d6dc5c1ace6eb93985f6f78a29bf98fb51
Lisa Angold <lisaangold@gmail.com>
2024-09-28 11:19:53 +0200

    shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button"
 import G6 from "./g6";
+import Link from "next/link";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center size-full sm:items-start">
+      <div className='flex flex-row gap-8'>
+        <Input placeholder='Suche ein Amt, Dezernat, ...'/>
+        <Button asChild >
+          <Link href="/">Suche</Link>
+        </Button>
+      </div>
       <G6 />
     </main>
   );
